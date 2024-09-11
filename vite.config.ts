4d541ea6cf5389cfcabac39dbf61957b05a51e00
ToniVaz <julianantonio.v@hotmail.com>
2024-09-11 15:18:59 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "/src", 
+      "@": "/src", // Configura el alias para que apunte a la carpeta src
     },
   },
 });
