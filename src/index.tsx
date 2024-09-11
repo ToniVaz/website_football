6f576e33df9b527ea9f574e8093f1dfd894206c7
ToniVaz <julianantonio.v@hotmail.com>
2024-09-11 15:06:00 -0300

    repaired route

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { AxiosFetch } from "../infrastructure/http/axiosConfig.js";
+import App from "./presentation/App.jsx";
+import { AxiosFetch } from "./infrastructure/http/axiosConfig.js";
 import { SWRConfig } from "swr";
-import "../global-styles/index.css";
+import "./global-styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <SWRConfig value={{ fetcher: AxiosFetch }}>
